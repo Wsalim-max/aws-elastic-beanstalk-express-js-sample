596827b6765bb0c2274551160eb75b2edf7c9160
Wsalim-max <80176114+Wsalim-max@users.noreply.github.com>
2021-03-06 15:38:10 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I am C00L with AWS! Yea Baby!!'));
+app.get('/', (req, res) => res.send('DevOps is Aw3$omE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
